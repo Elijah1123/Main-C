7a6fee536bd430046547f004fb1384e6169feb73
Elijah1123 <elijahmzalendo659@gmail.com>
2026-05-06 13:36:44 +0300

    initialize

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+void init() {
+    printf("System Initializing...\n");
+    printf("I like pizza\n");
+    printf("It's realy good!.....");
+}
+
+
+int main() {
+  
+    init(); 
+
+    return 0; 
+}
